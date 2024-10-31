9397fc499797dc57fb4473cf1104eb0a80b05087
krenak <aaa_lab@naver.com>
2024-10-31 00:51:53 -0300

    Last update libmatriz.py testaMat.py

--- 2024-2/prog2/exercicios/arquivo/libmatriz.py
+++ 2024-2/prog2/exercicios/arquivo/libmatriz.py
@@ -55,63 +55,6 @@ def salvaMatriz(m, nome_arquivo):
 
     arquivo.close()
 
-def insere(m, l, c, inserida):
-    # definindo a matriz menor a ser inserida
-    qtdL = int(input("qtd de linhas a inserir: "))
-    qtdC = int(input("qtd de colunas a inserir: "))
-    matInserida = []
-    linhaMat = []
-    for linhas in range(qtdL):
-        for colunas in range(qtdC):
-            linhaMat.append(inserida)
-        matInserida.append(linhaMat)
-        linhaMat = []
-
-    # teste de dimensão da matriz a ser inserida
-    if (l + qtdL) > (l + len(m)):
-        qtdL = len(m) - l
-    if (c + qtdC) > (len(m[0]) - c):
-        qtdC = len(m[0]) - c
-
-    # recriando a matriz com a matriz menor inserida
-    novaMatriz = []
-    for i in range(len(m)):
-        # condição onde a linha matriz princip é igual a linha de inserir matriz
-        if i >= l and i <= qtdL:
-            # início da matriz principal
-            for j in range(0, c):
-                linhaMat.append(m[i][j])
-
-            # ponto inicial (coluna) onde insere matriz menor
-            pos = 0 # posição da linha da matriz menor
-            for k in range(qtdC):
-                linhaMat.append(matInserida[pos][k])
-                pos += 1
-
-            # fim da matriz principal
-            for z in range(c + qtdC, len(m)):
-                linhaMat.append(m[i][z])
-            novaMatriz.append(linhaMat)
-        else:
-            novaMatriz.append(m[i])
-        linhaMat = []
-
-    return novaMatriz
-
-def deslocaEsq(matriz):
-    novaCol = []
-    novaMatriz = []
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            # print(matriz[j][i])
-            novaCol.append(matriz[j][i])
-            if j == 0:
-                del matriz[j]
-            novaMatriz.append(novaCol)
-            novaCol = []
-
-    print(novaMatriz)
-
 
 def testeLogico(m, x, y, qtdC, qtdL): # sugestao do Ernani
     qL = len(m)
@@ -157,11 +100,142 @@ def geraMat(lin, col, minimo, maximo):
         matriz.append(matrizLinhas)
     return matriz
 
+def insere(matriz, l, c, inserida):
+    # definindo a matriz menor a ser inserida
+    qtdL = int(input("qtd de linhas a inserir: "))
+    qtdC = int(input("qtd de colunas a inserir: "))
+    matInserida = []
+    linhaMat = []
+    for linhas in range(qtdL):
+        for colunas in range(qtdC):
+            linhaMat.append(inserida)
+        matInserida.append(linhaMat)
+        linhaMat = []
+
+    # teste de dimensão da matriz a ser inserida
+    # é importante perceber que o teste se refere 
+    # a posição do tamanho e não ao tamanho total
+    if (l + qtdL) > (len(matriz) - 1):       # posicao compr linha total - posicao ultima linha
+        qtdL = qtdL - ((l + qtdL) - len(matriz))
+    if (c + qtdC) > (len(m[0])- 1):    # posicao compr coluna total - posicao ultima coluna
+        qtdC = qtdC - ((c + qtdC) - len(matriz[0]))
+
+    # versão ernani
+    # segunda versao da matriz inserida
+    z = 0
+    for i in range(l, l + qtdL):
+        w = 0
+        for j in range(c, c + qtdC):
+            matriz[i][j] = matInserida[z][w]
+            w += 1
+        z += 1
+
+#    # minha versão
+#    # recriando a matriz com a matriz menor inserida
+#    novaMatriz = []
+#    for i in range(len(m)):
+#        # condição onde a linha matriz princip é igual a linha de inserir matriz
+#        if i >= l and i <= qtdL:
+#            # início da matriz principal
+#            for j in range(0, c):
+#                linhaMat.append(m[i][j])
+#
+#            # ponto inicial (coluna) onde insere matriz menor
+#            pos = 0 # posição da linha da matriz menor
+#            for k in range(qtdC):
+#                linhaMat.append(matInserida[pos][k])
+#                pos += 1
+#
+#            # fim da matriz principal
+#            for z in range(c + qtdC, len(m)):
+#                linhaMat.append(m[i][z])
+#            novaMatriz.append(linhaMat)
+#        else:
+#            novaMatriz.append(m[i])
+#        linhaMat = []
+
+    return matriz
+
+def deslocaEsq(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if j != (len(matriz[i]) - 1):
+                matriz[i][j] = matriz[i][j + 1]
+            else:
+                matriz[i][j] = 0
+
+    return matriz
+
+def deslocaDir(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i]) - 1, -1, -1):
+            matriz[i][j] = matriz[i][j - 1]
+            if j == 0:
+                matriz[i][j] = 0
+
+    return matriz
+
+# função de rotação da matriz à direita
+def rotDir(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i]) - 1, -1, -1):
+            if j == (len(matriz[i]) - 1):
+                x = matriz[i][j]
+            matriz[i][j] = matriz[i][j - 1]
+            if j == 0:
+                matriz[i][j] = x
+
+    return matriz
+
+# função de rotação da matriz à esquerda
+def rotEsq(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if j == 0:
+                x = matriz[i][j]
+            if j != (len(matriz[i]) - 1):
+                matriz[i][j] = matriz[i][j + 1]
+            else:
+                matriz[i][j] = x
+
+    return matriz
+
+def vizinhos(matriz, posL, posC): 
+    # posL = posição do elem na linha;
+    # posC = posição do elem na coluna;
+    linhaCima = posL - 1
+    linhaBaixo = posL + 2
+    colEsq = posC - 1
+    colDir = posC + 2
+    vizinhos = []
+
+    for linha in range(linhaCima, linhaBaixo):
+        if linha == linhaCima:
+            for coluna in range(colEsq, colDir):
+                vizinhos.append(matriz[linha][coluna])
+        if linha == (linhaCima + 1):
+            n4 = matriz[linha][colDir - 1]
+            vizinhos.append(n4)
+            n8 = matriz[linha][colEsq]
+        if linha == (linhaCima + 2):
+            for i in range(colDir - 1, colEsq - 1, - 1):
+                vizinhos.append(matriz[linha][i])
+            vizinhos.append(n8)
+
+    return vizinhos
+
+
+# função de impressão de saída
 def formatImpressao(matriz):
     for linhas in range(len(matriz)):
         for colunas in matriz[linhas]:
             print("%1d" % colunas, " ", end="")
         print()
+
+# função de impressão vizinhos
+def impressaoVizinhos(matriz):
+    for numeros in matriz:
+        print("%1d" % numeros, " ", end="")
 
 #def formatImpressao2(matriz, lin, col):                    # l = numero de linhas da matriz
 #    mat = ""
--- 2024-2/prog2/exercicios/arquivo/testaMat.py
+++ 2024-2/prog2/exercicios/arquivo/testaMat.py
@@ -1,18 +1,41 @@
 import libmatriz as lb
 
 def main():
-    mat = lb.geraMat(4,6,1,9)
+    mat = lb.geraMat(5,6,1,9)
     #mat = lb.loadMatriz("matrizTeste.txt")
     lb.formatImpressao(mat)
+
+    print()
 
 #    salvar = str(input("qual o caminho e nome do arquivo a ser salvo? "))
 #    lb.salvaMatriz(mat, salvar)
 
-    opcaoInsere= int(input("int a inserir na matriz: "))
-    inserida = lb.insere(mat, 1, 1, opcaoInsere)
-    lb.formatImpressao(inserida)
-    # mat = lb.extraiMat(m, l, c, qtdL, qtdC)
-    #matEx = lb.extraiMat(mat, 1, 2, 3, 3)
-    #lb.formatImpressao(matEx)
+#    mat = lb.extraiMat(m, l, c, qtdL, qtdC)
+#    matEx = lb.extraiMat(mat, 1, 2, 3, 3)
+#    lb.formatImpressao(matEx)
+
+#    opcaoInsere= int(input("int a inserir na matriz: "))
+#    posX= int(input("posição inicial da linha: "))
+#    posY= int(input("posição inicial da coluna: "))
+#    inserida = lb.insere(mat, posX, posY, opcaoInsere)
+#    lb.formatImpressao(inserida)
+
+#    dEsq = lb.deslocaEsq(mat)
+#    lb.formatImpressao(dEsq)
+
+#    dDir = lb.deslocaDir(mat)
+#    lb.formatImpressao(dDir)
+
+#    rDir = lb.rotDir(mat)
+#    lb.formatImpressao(rDir)
+
+#    rEsq = lb.rotEsq(mat)
+#    lb.formatImpressao(rEsq)
+
+    viz = lb.vizinhos(mat, 1, 2)
+    lb.impressaoVizinhos(viz)
+
+
 
 main()
+
