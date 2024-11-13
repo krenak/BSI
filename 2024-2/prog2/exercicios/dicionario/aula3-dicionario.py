d2022dc5f55fd8f9496e2d428a98f4ead8de9ef5
krenak <aaa_lab@naver.com>
2024-11-12 23:29:57 -0300

    update dicionario - prog2

---
+++ 2024-2/prog2/exercicios/dicionario/aula3-dicionario.py
@@ -0,0 +1,24 @@
+'''
+aula 3 - dicionario
+07/11/24
+
+==== encontrar chave ====
+> d.get("SP", "elemento não encontrado")
+> 
+
+==== uso de tuplas como chaves complexas ou chaves compostas ====
+# localizar uma casa, de forma única, dentro de um bairro
+> casa = {("29000123", 10): "Monteiro", ("29000456", 40): "Azeredo", ("29000123", 30): "Guimarães"}
+    for kt in casa:
+        if kt[0] == "29000123":
+            print(casa[k])
+> Monteiro
+  Guimaraes
+
+desafio 1: incluir caracteres em maiusculo
+desafio 2: 
+
+'''
+
+
+
