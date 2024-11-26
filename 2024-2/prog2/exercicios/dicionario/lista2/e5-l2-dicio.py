0658fc9d4895fc3c8ba199d0e0253b158eaa2811
krenak <aaa_lab@naver.com>
2024-11-26 09:06:25 -0300

    update e5-l2-dicio.py - prog2

--- 2024-2/prog2/exercicios/dicionario/lista2/e5-l2-dicio.py
+++ 2024-2/prog2/exercicios/dicionario/lista2/e5-l2-dicio.py
@@ -1,18 +1,7 @@
-def comprimento(d):
-    comp = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
-    palavras = d.split()    
-    for p in range(len(palavras)):
-        for l in palavras[p]:
-            if 
-    #     valores = d[k]
-    #     cont = 0
-    #     for c in valores:
-    #         cont += 1
-    #     comp[valores] = cont
-    # return comp
+import comprimentoL2 as comp
 
 def main():
-	s = "o dia está maravilhoso"
-	contagem = comprimento(s)
+	s = "o pão de ló"
+	contagem = comp.comprimento(s)
 	print(contagem)
 main()
