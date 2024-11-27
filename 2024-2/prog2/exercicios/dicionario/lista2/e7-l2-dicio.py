8bb0373609f6d5a2791c36cc90fad59165b61fb0
krenak <aaa_lab@naver.com>
2024-11-27 09:10:52 -0300

    add e6-e7-l2-dicio.py - prog2

--- 2024-2/prog2/exercicios/dicionario/lista2/e7-l2-dicio.py
+++ 2024-2/prog2/exercicios/dicionario/lista2/e7-l2-dicio.py
@@ -1,3 +1,23 @@
-def main():
+def agendaTel(l):
+	dicioTel = {}
+	for i in l:
+		dicioTel[i[0]] = {i[1], i[2]}
+	return dicioTel
 
+def loadFile(arquivo):
+	agenda = []
+	arq = open(arquivo, "rt")
+	linha = arq.readline().strip()
+	while linha != "":
+		lista = linha.split(", ")
+		agenda.append(lista)
+		linha = arq.readline().strip()
+
+	return agenda
+
+def main():
+	agenda = loadFile("bdagendatelefonica.txt")
+	d = agendaTel(agenda)
+	print(d)
+	
 main()
