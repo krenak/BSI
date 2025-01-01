1807a23c12905ea19db4d595e134f7138f1c9559
krenak <aaa_lab@naver.com>
2025-01-01 20:51:25 -0300

    update e2-tad - prog2

--- 2024-2/prog2/exercicios/TAD/e2/e2-eq2ograu.py
+++ 2024-2/prog2/exercicios/TAD/e2/e2-eq2ograu.py
@@ -2,7 +2,7 @@
 CONSTRUA UMA EQUAÇÃO DO SEGUNDO GRAU. FAÇA UMA ESCOLHA PARA A IMPLEMENTAÇÃO E
 CONSTRUA A SEGUINTE INTERFACE:
 
-a) new_eq(ca, cb, cc)				#CONSTRUTORA
+a) new_eq(ca, cb, cc)                #CONSTRUTORA
 b) getA(tad), getB(tad), getC(tad)
 c) quant_raizes(tad)
 d) getRaiz1(tad)
@@ -17,27 +17,27 @@ A IDEIA PRINCIPAL DO TAD É A ABSTRAÇÃO E O REUSO.
 import eq2grau as eq
 
 def main():
-	eq = eq.new_eq(1, -1, -12)
-	print("Quantidade de raízes: ", eq.quant_raizes(eq))
-	print("Raíz 1: ", eq.getRaiz1(eq))
-	print("Raíz 2: ", eq.getRaiz2(eq))
+    eq = eq.new_eq(1, -1, -12)
+    print("Quantidade de raízes: ", eq.quant_raizes(eq))
+    print("Raíz 1: ", eq.getRaiz1(eq))
+    print("Raíz 2: ", eq.getRaiz2(eq))
 
 main()
 
 '''
 constA = int(input("Insira a constante "a" junto a seu sinal (+ ou -): "))
-	constB = int(input("Insira a constante "b" junto a seu sinal (+ ou -): "))
-	constC = int(input("Insira a constante "c" junto a seu sinal (+ ou -): "))
-	opcoes = str(input('defina o que voce precisa: (a) nova eq; (b) coeficientes; (c) quantidade de raízes; (d) raiz 1; (e) raiz 2'))
-	equacao = eq.new_eq(constA, constB, constC)
+    constB = int(input("Insira a constante "b" junto a seu sinal (+ ou -): "))
+    constC = int(input("Insira a constante "c" junto a seu sinal (+ ou -): "))
+    opcoes = str(input('defina o que voce precisa: (a) nova eq; (b) coeficientes; (c) quantidade de raízes; (d) raiz 1; (e) raiz 2'))
+    equacao = eq.new_eq(constA, constB, constC)
 
-	if opcoes == 'a':
-		constA = int(input("Insira a constante "a" junto a seu sinal (+ ou -): "))
-		constB = int(input("Insira a constante "b" junto a seu sinal (+ ou -): "))
-		constC = int(input("Insira a constante "c" junto a seu sinal (+ ou -): "))
-		eq.new_eq(constA, constB, constC)
-	elif opcoes == 'b':
-		eq.getA()
-		eq.getA()
-		eq.getA()
+    if opcoes == 'a':
+        constA = int(input("Insira a constante "a" junto a seu sinal (+ ou -): "))
+        constB = int(input("Insira a constante "b" junto a seu sinal (+ ou -): "))
+        constC = int(input("Insira a constante "c" junto a seu sinal (+ ou -): "))
+        eq.new_eq(constA, constB, constC)
+    elif opcoes == 'b':
+        eq.getA()
+        eq.getA()
+        eq.getA()
 '''
--- 2024-2/prog2/exercicios/TAD/e2/eq2grau.py
+++ 2024-2/prog2/exercicios/TAD/e2/eq2grau.py
@@ -3,52 +3,52 @@ eq2grau.py
 """
 
 def new_eq(ca, cb, cc):
-	return (ca, cb, cc)
+    return (ca, cb, cc)
 
 def new_eq2(eq): # exercicio de parsing: recebe uma string como 5x2 + 3x + 1
-	pass
+    pass
 
 #=========================================
 
 def getA(tad):
-	return tad[0]
+    return tad[0]
 
 def getB(tad):
-	return tad[1]
+    return tad[1]
 
 def getC(tad):
-	return tad[2]
+    return tad[2]
 
 def quant_raizes(tad):
-	pass
+    pass
 def getRaiz1(tad):
-	pass
+    pass
 def getRaiz2(tad):
-	pass
+    pass
 
 
 '''
 itensEq = {}
-	print(f"Equação escolhida: {ca}x²{cb}x{cc}=0")
-	print()
-	if ca == 0:
-		print(f"A equação escohida não é de 2o grau, pois o coeficiente 'a' é igual a == {ca}")
-		return None
-	else:
-		delta = (cb ** 2) - (4 * ca * cc)
-		print(f"O delta encontrado foi: {delta}")
-		if delta > 0:
-			x1 = ((-1)*cb + (delta**0.5)) / 2 * ca
-			xb = ((-1)*cb - (delta**0.5)) / 2 * ca
-			print(f"As raízes encontradas foram x1: {x1} e x2: {x2}")
-		elif delta == 0:
-			x1 = ((-1)*cb + (delta**0.5)) / 2 * ca
-			x2 = None
-			print(f"A raíz encontrada foi x: {x1}")
-		else:
-			print(f"A solução da equação {ca}x²{cb}x{cc}=0 não contém raízes reais: {delta} < 0.")
-			return None
-	itensEq["eq"] = {"coefA": ca, "coefB": cb, "coefC": cc}
-	itensEq["delta"] = {"delta": delta}
-	itensEq["raizes"] = {"x1": x1, "x2": x2}
+    print(f"Equação escolhida: {ca}x²{cb}x{cc}=0")
+    print()
+    if ca == 0:
+        print(f"A equação escohida não é de 2o grau, pois o coeficiente 'a' é igual a == {ca}")
+        return None
+    else:
+        delta = (cb ** 2) - (4 * ca * cc)
+        print(f"O delta encontrado foi: {delta}")
+        if delta > 0:
+            x1 = ((-1)*cb + (delta**0.5)) / 2 * ca
+            xb = ((-1)*cb - (delta**0.5)) / 2 * ca
+            print(f"As raízes encontradas foram x1: {x1} e x2: {x2}")
+        elif delta == 0:
+            x1 = ((-1)*cb + (delta**0.5)) / 2 * ca
+            x2 = None
+            print(f"A raíz encontrada foi x: {x1}")
+        else:
+            print(f"A solução da equação {ca}x²{cb}x{cc}=0 não contém raízes reais: {delta} < 0.")
+            return None
+    itensEq["eq"] = {"coefA": ca, "coefB": cb, "coefC": cc}
+    itensEq["delta"] = {"delta": delta}
+    itensEq["raizes"] = {"x1": x1, "x2": x2}
 '''
